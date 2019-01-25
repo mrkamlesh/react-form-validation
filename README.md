ea973d529ff77041ab4c99853402937b2b69e678
Kamlesh Kumar <mrkamleshkumar2010@gmail.com>
2019-01-25 10:56:15 +0530

    validation update

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 This is a simple demo app for showing how to do basic form validation in React.
 
-[See the app running on Heroku](https://reactform.herokuapp.com).
+[See the app running on local](http://localhost:3000/).
 
 It includes a sign up form with email and password input fields and a sign up button.
-
-For more on how to generalise the validations, have a look at [The Complete React on Rails Course](learnetto.com/users/hrishio/courses/the-complete-react-on-rails-5-course).
--- src/Form.js
+++ src/Form.js
@@ -3,23 +3,32 @@ import { FormErrors } from './FormErrors';
 import './Form.css';
 
 class Form extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
-      formErrors: {email: '', password: ''},
+      formErrors: { email: '', password: '' },
       emailValid: false,
       passwordValid: false,
-      formValid: false
+      formValid: false,
+      isfFormSubmited: false
     }
   }
 
   handleUserInput = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    this.setState({[name]: value},
-                  () => { this.validateField(name, value) });
+    this.setState({ [name]: value },
+      () => { this.validateField(name, value) });
+  }
+
+  handleFormSubmit = (e) => {
+    this.setState({ isfFormSubmited: true });
+    console.log("form email value: ", this.state.email);
+    console.log("form password value: ", this.state.password);
+    
+    e.preventDefault();
   }
 
   validateField(fieldName, value) {
@@ -27,53 +36,76 @@ class Form extends Component {
     let emailValid = this.state.emailValid;
     let passwordValid = this.state.passwordValid;
 
-    switch(fieldName) {
+    switch (fieldName) {
       case 'email':
         emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
         fieldValidationErrors.email = emailValid ? '' : ' is invalid';
         break;
       case 'password':
-        passwordValid = value.length >= 6;
-        fieldValidationErrors.password = passwordValid ? '': ' is too short';
+        passwordValid = value.length >= 3;
+        fieldValidationErrors.password = passwordValid ? '' : ' is too short';
         break;
       default:
         break;
     }
-    this.setState({formErrors: fieldValidationErrors,
-                    emailValid: emailValid,
-                    passwordValid: passwordValid
-                  }, this.validateForm);
+
+    this.setState({
+      formErrors: fieldValidationErrors,
+      emailValid: emailValid,
+      passwordValid: passwordValid
+    }, this.validateForm);
   }
 
   validateForm() {
-    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+    this.setState({ formValid: this.state.emailValid && this.state.passwordValid });
   }
 
   errorClass(error) {
-    return(error.length === 0 ? '' : 'has-error');
+    return (error.length === 0 ? '' : 'has-error');
   }
 
-  render () {
+  hasInputError(inputField) {
+    return (inputField.length === 0 ? false : true);
+  }
+
+  hasFormError(emailError, passwordError) {
+    if (emailError.length === 0 && passwordError.length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  render() {
     return (
-      <form className="demoForm">
+      <form className="demoForm" onSubmit={this.handleFormSubmit}>
         <h2>Sign up</h2>
-        <div className="panel panel-default">
-          <FormErrors formErrors={this.state.formErrors} />
-        </div>
+        {this.hasFormError(this.state.formErrors.email, this.state.formErrors.password) && (
+          <div className="panel panel-default">
+            <FormErrors formErrors={this.state.formErrors} />
+          </div>
+        )}
+
         <div className={`form-group ${this.errorClass(this.state.formErrors.email)}`}>
           <label htmlFor="email">Email address</label>
           <input type="email" required className="form-control" name="email"
             placeholder="Email"
             value={this.state.email}
-            onChange={this.handleUserInput}  />
+            onChange={this.handleUserInput} />
+            {this.hasInputError(this.state.formErrors.email) && (
+            <small className="form-text has-error-input">Please input valid email.</small>
+            )}
         </div>
+        
+
         <div className={`form-group ${this.errorClass(this.state.formErrors.password)}`}>
           <label htmlFor="password">Password</label>
           <input type="password" className="form-control" name="password"
             placeholder="Password"
             value={this.state.password}
-            onChange={this.handleUserInput}  />
+            onChange={this.handleUserInput} />
         </div>
+        
         <button type="submit" className="btn btn-primary" disabled={!this.state.formValid}>Sign up</button>
       </form>
     )
